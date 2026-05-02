6ea0c6d4f2a75afc736dcc7797d2c8ae21b8b0d9
ShotaGhoona <shota.yamashita@ghoona.com>
2026-05-02 20:32:23 +0900

    style: CardとDialogにセクション区切り線を追加

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -59,7 +59,7 @@ const DialogHeader = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
+      "flex flex-col space-y-1.5 text-center sm:text-left -mx-6 px-6 pb-4 border-b",
       className
     )}
     {...props}
@@ -73,7 +73,7 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 -mx-6 px-6 pt-4 border-t",
       className
     )}
     {...props}
